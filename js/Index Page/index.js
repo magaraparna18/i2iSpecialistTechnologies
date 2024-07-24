e0eecff30ef12060eb886e6c19a62944b719da9b
magaraparna <magaraparna18@gmail.com>
2024-07-24 23:28:43 +0530

    changes added

--- js/Index Page/index.js
+++ js/Index Page/index.js
@@ -2,3 +2,28 @@ document.querySelector('.rs-menu-toggle').addEventListener('click', function() {
     this.classList.toggle('active');
     document.querySelector('.rs-menu').classList.toggle('d-none');
 });
+
+// Partner section 
+$(document).ready(function(){
+    $('.rs-carousel').owlCarousel({
+        loop: true,
+        margin: 30,
+        autoplay: true,
+        autoplayTimeout: 1, // Time between transitions (1 second)
+        autoplayHoverPause: false, // Prevent pausing on hover
+        smartSpeed: 2, // Speed of the transition
+        dots: false,
+        nav: false,
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+});
